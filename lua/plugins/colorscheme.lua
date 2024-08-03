a197aee9b5123267c77bbf94d664e102bf703f3b
Tyler Limbach <tylimbach@gmail.com>
2024-08-03 01:35:42 -0500

    set gruvbox term colors and bash toggleterm

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,70 @@
 vim.o.bg = "light"
 
+local function set_terminal_colors()
+  local colors = {
+    dark_grey = "#282828",
+    red = "#cc241d",
+    green = "#98971a",
+    yellow = "#d79921",
+    blue = "#458588",
+    purple = "#b16286",
+    aqua = "#689d6a",
+    light_grey = "#a89984",
+    bright_grey = "#928374",
+    bright_red = "#fb4934",
+    bright_green = "#b8bb26",
+    bright_yellow = "#fabd2f",
+    bright_blue = "#83a598",
+    bright_purple = "#d3869b",
+    bright_aqua = "#8ec07c",
+    bright_white = "#ebdbb2",
+  }
+
+  -- Set terminal colors using Vimscript commands
+  vim.cmd(
+    string.format(
+      [[
+        let g:terminal_color_0 = '%s'
+        let g:terminal_color_1 = '%s'
+        let g:terminal_color_2 = '%s'
+        let g:terminal_color_3 = '%s'
+        let g:terminal_color_4 = '%s'
+        let g:terminal_color_5 = '%s'
+        let g:terminal_color_6 = '%s'
+        let g:terminal_color_7 = '%s'
+        let g:terminal_color_8 = '%s'
+        let g:terminal_color_9 = '%s'
+        let g:terminal_color_10 = '%s'
+        let g:terminal_color_11 = '%s'
+        let g:terminal_color_12 = '%s'
+        let g:terminal_color_13 = '%s'
+        let g:terminal_color_14 = '%s'
+        let g:terminal_color_15 = '%s'
+    ]],
+      colors.dark_grey,
+      colors.red,
+      colors.green,
+      colors.yellow,
+      colors.blue,
+      colors.purple,
+      colors.aqua,
+      colors.light_grey,
+      colors.bright_grey,
+      colors.bright_red,
+      colors.bright_green,
+      colors.bright_yellow,
+      colors.bright_blue,
+      colors.bright_purple,
+      colors.bright_aqua,
+      colors.bright_white
+    )
+  )
+end
+
+-- Optionally, set this function to run after your colorscheme is loaded,
+-- or call it directly at the appropriate place in your configuration.
+set_terminal_colors()
+
 return {
   -- lush used by zenbones
   { "rktjmp/lush.nvim" },
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -84,7 +84,7 @@ return {
       direction = "horizontal" or "vertical" or "window" or "float",
       -- direction = "vertical",
       close_on_exit = true, -- close the terminal window when the process exits
-      -- shell = vim.o.shell, -- change the default shell
+      shell = "bash", -- change the default shell
       -- This field is only relevant if direction is set to 'float'
       -- float_opts = {
       --   -- The border key is *almost* the same as 'nvim_open_win'
