0aee6495ccaf328a4b909e6c055b003aad072f46
Tyler Limbach <tylimbach@gmail.com>
2024-08-11 19:14:02 -0500

    neovide scale keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local map = vim.keymap.set
+
 require("neo-tree").setup({
   filesystem = {
     window = {
@@ -11,3 +13,14 @@ require("neo-tree").setup({
     },
   },
 })
+
+vim.g.neovide_scale_factor = 1.0
+local change_scale_factor = function(delta)
+  vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+end
+vim.keymap.set("n", "<C-=>", function()
+  change_scale_factor(1.25)
+end)
+vim.keymap.set("n", "<C-->", function()
+  change_scale_factor(1 / 1.25)
+end)
