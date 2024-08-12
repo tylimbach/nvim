6f78a427420d184dc15a66a24282f6018aaf33e6
Tyler Limbach <tylimbach@gmail.com>
2024-08-11 21:01:54 -0500

    neovide reduce scale factor 1.1

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,8 +19,8 @@ local change_scale_factor = function(delta)
   vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
 end
 vim.keymap.set("n", "<C-=>", function()
-  change_scale_factor(1.25)
+  change_scale_factor(1.1)
 end)
 vim.keymap.set("n", "<C-->", function()
-  change_scale_factor(1 / 1.25)
+  change_scale_factor(1 / 1.1)
 end)
