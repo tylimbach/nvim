58a8c6392f4c4b2687086e61a18e5fd775f89b1b
Tyler Limbach <tlimbach@wolve.com>
2025-02-23 16:49:12 -0600

    update to lazyvim edition 8: snacks picker and explorer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,20 +4,6 @@
 
 local map = vim.keymap.set
 
-require("neo-tree").setup({
-  filesystem = {
-    window = {
-      mappings = {
-        ["/"] = "noop", -- disable fuzzy finder, should fallback to normal search
-      },
-    },
-    filtered_items = {
-      visible = true,
-    },
-    max_items = 2000,
-  },
-})
-
 vim.g.neovide_scale_factor = 1.0
 local change_scale_factor = function(delta)
   vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
