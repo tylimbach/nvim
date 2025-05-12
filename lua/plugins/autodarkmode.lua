5f37374910b72fcc3a0cb4862b51051ea8218a54
Tyler Limbach <tlimbach@wolve.com>
2025-05-12 12:54:08 -0500

    fix wsl cursor color overriding terminal

--- lua/plugins/autodarkmode.lua
+++ lua/plugins/autodarkmode.lua
@@ -6,11 +6,11 @@ return {
       update_interval = 1000,
       set_dark_mode = function()
         vim.api.nvim_set_option_value("background", "dark", {})
-        vim.mycolors.set_terminal_colors()
+        -- vim.mycolors.set_terminal_colors()
       end,
       set_light_mode = function()
         vim.api.nvim_set_option_value("background", "light", {})
-        vim.mycolors.set_terminal_colors()
+        -- vim.mycolors.set_terminal_colors()
       end,
 	fallback = "dark",
     },
