3fbcf416e83c5f3fd7fbdd62b5bba27135e0b618
tylimbach <tylimbach@gmail.com>
2025-08-15 09:46:23 -0500

    use jetls over julia_ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,17 @@
+vim.lsp.config.jetls = {
+	cmd = {
+		"julia",
+		"--startup-file=no",
+		"--history-file=no",
+		"--project=/Users/ty/dev/JETLS.jl",
+		"/Users/ty/dev/JETLS.jl/runserver.jl",
+	},
+	filetypes = { "julia" },
+	root_markers = { "Project.toml", "JuliaProject.toml", ".git" },
+}
+
+vim.lsp.enable({ "jetls" })
+
 return {
 	-- roslyn lsp wrapper for csharp
 	{
@@ -60,21 +74,44 @@ return {
 				-- 	},
 				-- },
 				julials = {
+					enabled = false,
 					mason = false,
 					cmd = {
 						"julia",
-						"--project=@nvim-lspconfig",
+						"--project=".."~/.julia/environments/lsp/",
 						"--startup-file=no",
 						"--history-file=no",
 						"-e", [[
-							using LanguageServer, SymbolServer;
-							env = get(ENV, "JULIA_PROJECT", "@nvim-lspconfig");
-							server = LanguageServer.LanguageServerInstance(stdin, stdout, env);
-							server.runlinter = true;
-							run(server);
+							using Pkg
+							Pkg.instantiate()
+							using LanguageServer
+						depot_path = get(ENV, "JULIA_DEPOT_PATH", "")
+						project_path = let
+							dirname(something(
+								## 1. Finds an explicitly set project (JULIA_PROJECT)
+								Base.load_path_expand((
+									p = get(ENV, "JULIA_PROJECT", nothing);
+										p === nothing ? nothing : isempty(p) ? nothing : p
+									)),
+										## 2. Look for a Project.toml file in the current working directory,
+										##    or parent directories, with $HOME as an upper boundary
+										Base.current_project(),
+										## 3. First entry in the load path
+										get(Base.load_path(), 1, nothing),
+										## 4. Fallback to default global environment,
+										##    this is more or less unreachable
+									Base.load_path_expand("@v#.#"),
+								))
+							end
+									@info "Running language server" VERSION pwd() project_path depot_path
+									server = LanguageServer.LanguageServerInstance(stdin, stdout, project_path, depot_path)
+						server.runlinter = true
+							run(server)
 						]]
 					},
-					filetypes = { "julia" },
+					filetypes = { 'julia' },
+					root_markers = { "Project.toml", "JuliaProject.toml" },
+					settings = {}
 					-- root_dir = require("lspconfig.util").find_git_ancestor,
 				},
 				-- xml
