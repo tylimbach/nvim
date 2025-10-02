890834c477b22b97c277050d3159af67ba7ed572
Tyler Limbach <tlimbach@wolve.com>
2025-10-01 19:05:58 -0500

    add mcphub

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -63,6 +63,16 @@ return {
 						})
 					end,
 				},
+				tools = {
+					wolverine_mcp = {
+						name = "Wolverine MCP Tools",
+						description = "Wolverine internal firm tools and services",
+						url = "http://127.0.0.1:8100/mcp",
+						headers = {
+							["Content-Type"] = "application/json"
+						},
+					}
+				},
 				prompt_library = {
 					Docusaurus = {
 						strategy    = "chat",
@@ -158,6 +168,21 @@ I'm also sharing my `config.lua` file which I'm mapping to the `configuration` s
 								files_ls = {},
 								files_rm = {},
 							},
+						},
+					},
+					mcphub = {
+						callback = "mcphub.extensions.codecompanion",
+						opts = {
+							-- MCP Tools 
+							make_tools = true,              -- Make individual tools (@server__tool) and server groups (@server) from MCP servers
+							show_server_tools_in_chat = true, -- Show individual tools in chat completion (when make_tools=true)
+							add_mcp_prefix_to_tool_names = false, -- Add mcp__ prefix (e.g `@mcp__github`, `@mcp__neovim__list_issues`)
+							show_result_in_chat = true,      -- Show tool results directly in chat buffer
+							format_tool = nil,               -- function(tool_name:string, tool: CodeCompanion.Agent.Tool) : string Function to format tool names to show in the chat buffer
+							-- MCP Resources
+							make_vars = true,                -- Convert MCP resources to #variables for prompts
+							-- MCP Prompts 
+							make_slash_commands = true,      -- Add MCP prompts as /slash commands
 						},
 					},
 				},
